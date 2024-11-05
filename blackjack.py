11f5763a4bfbcb001d802c9a90ad261acff384c9
moe.tsuchiya <moet.therese@gmail.com>
2024-11-05 18:40:55 +0900

    おけ

---
+++ blackjack.py
@@ -0,0 +1,7 @@
+import random
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 'Joker']
+
+random_two = random.sample(cards, 2)
+
+print(random_two)
--- janken.py
+++ janken.py
@@ -1,21 +1,37 @@
 import random
+def janken_game(): 
+        count = 0
+        while count < 3:
+            print('３回勝つまでじゃんけん')
+            print('ぐー: 0, ちょき:1, ぱー:2')
+            my_hand = int(input())
+            if my_hand == 0:
+                print('あなたの手はぐーです')
+            elif my_hand == 1:
+                print('あなたの手はちょきです')
+            elif my_hand == 2:
+                print('あなたの手はぱーです')
 
-def janken_game():     
-    print('じゃんけん...')
+            hands = [0, 1, 2]
+            pc_hand = random.choice(hands)
+            if pc_hand == 0:
+                print('相手の手はぐーです')
+            elif pc_hand == 1:
+                print('相手の手はちょきです')
+            elif pc_hand == 2:
+                print('相手の手はぱーです')
+            else:
+                print('0か1か2のどれかを入力してください！')
+                continue
 
-    my_hand = int(input('ぐー: 0, ちょき:1, ぱー:2'))
-    print('あなたの手は' + str(my_hand))
 
-    hands = [0, 1, 2]
-    pc_hand = random.choice(hands)
-    print('相手の手は' + str(pc_hand))
-
-    if my_hand == pc_hand:
-        print('あいこで...')
-        janken_game()
-    elif (my_hand == 0 and pc_hand == 1) or (my_hand == 1 and pc_hand == 2) or (my_hand == 2 and pc_hand == 0):
-        print('あなたの勝ち！')
-    else:
-        print('負けです...ざんねん')
+            if my_hand == pc_hand:
+                print('あいこで...')
+                continue
+            elif (my_hand == 0 and pc_hand == 1) or (my_hand == 1 and pc_hand == 2) or (my_hand == 2 and pc_hand == 0):
+                count += 1
+                print('あなたの勝ち！' + str(count) + '回目です！')
+            else:
+                print('負けです...残念')
 
 janken_game()
