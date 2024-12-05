f7f2ce6315cdf6c3d70ab945207d2372ce0ef934
moe.tsuchiya <moet.therese@gmail.com>
2024-12-05 23:19:29 +0900

    追加

--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,12 @@ import random
 suits = ['♥', '♦', '♣', '♠']
 numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 cards = [f'{suit}{number}' for suit in suits for number in numbers]
+"""
+cards = []
+for suit in suits:
+    for number in numbers:
+        cards.append(f'{suit}{number}')
+"""
 random.shuffle(cards)
 dealer_cards = [cards.pop() for _ in range(2)]
 player_cards = [cards.pop() for _ in range(2)]
@@ -9,8 +15,47 @@ player_cards = [cards.pop() for _ in range(2)]
 print('あなたの手札は' + str(player_cards) + 'です')
 
 
-"""
-def stand():
-    print(your_cards)
-    print(dealer_cards)"""
+def numbering_cards(cards):
+    values = []
+    for card in cards:
+        rank  = card[1:]
+        if rank == 'A':
+            values.append(1)
+        elif rank in ['J', 'Q', 'K']:
+            values.append(10)
+        else:
+            values.append(int(rank))
+    return sum(values)
+
+player_num = numbering_cards(player_cards)
+print(player_num)
+
+while player_num <= 16:
+    player_cards = player_cards.append(cards.pop())
+    player_num = numbering_cards(player_cards)
+
+print(player_cards)
+print(player_num)
+    
+
+# 全て数字にする、合計を出す
+# a = 1 or 11
+# 10 j q k = 10
+
+# while 手札が16以下
+# なら　一枚引く
+
+# でないなら　引かない　
+#     hit or stand
+#         hit 
+#             一枚引く
+
+#         stand
+#             21に近いのは...勝ち負け
+
+# """
+# def stand():
+#     print(your_cards)
+#     print(dealer_cards)
+# """
 
